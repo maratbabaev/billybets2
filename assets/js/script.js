80cb8456a291965f1be5b78342c49fd5e57707fa
maratbabaev <you@example.com>
2025-02-17 17:35:12 +0300

    5

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("a").forEach((elem) => {
-	elem.onclick = function (e) {
-		e.preventDefault();
-		window.location.href = "https://redirecttraff.site/Q8mmdvr2";
-		return false;
-	};
-});
